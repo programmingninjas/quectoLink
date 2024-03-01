f29bd7acd6d8d9fd22f78fa36061fc035a6259d3
programmingninjas <programing.ninja0@gmail.com>
2024-03-02 02:40:42 +0530

    Removed Files

--- backend/routes/goalRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const { protect } = require('../middleware/authMiddleware')
-const { getGoals, setGoal, updateGoal, deleteGoal } = require('../controllers/goalController')
-const router = express.Router()
-
-router.route("/").get(protect,getGoals).post(protect,setGoal)
-router.route("/:id").delete(protect,deleteGoal).put(protect,updateGoal)
-
-module.exports = router
