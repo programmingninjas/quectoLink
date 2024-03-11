48b76e3b6806f6b077150f1d7560468949214568
Ayan Khan <programing.ninja0@gmail.com>
2024-03-11 14:12:38 +0530

    Update client.js

--- backend/cache/client.js
+++ backend/cache/client.js
@@ -2,7 +2,7 @@ const {Redis} = require('ioredis');
 
 const client = new Redis({
     port: 6379,
-    host: "quectolink-cache-1"
+    host: "cache"
 }); 
 
 //const client = new Redis()  use this when you are not using docker
