0f4335f917b527b58b2b69b60b8b7c6701860495
Ayan Khan <programing.ninja0@gmail.com>
2024-09-27 19:00:20 +0530

    Update entrypoint.sh

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -9,7 +9,7 @@ file=$(find /usr/share/nginx/html/assets -type f -name 'index*.js')
 # Check if the file was found
 if [ -n "$file" ]; then
     # Replace localhost:5000 with the value of $IP in the found file
-    sed -i "s/localhost:5000/${IP}/g" "$file"
+    sed -i "s|http://localhost:5000|${IP}|g" "$file"
 else
     echo "No index*.js file found in /usr/share/nginx/html/assets"
 fi
